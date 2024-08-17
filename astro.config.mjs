9107fa125f09f5fa297f41a384bb5c1d80bfa582
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-08-18 02:11:43 +0530

    init

---
+++ astro.config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: 'Notes',
+    social: {
+      github: 'https://github.com/pranshuj73/fsn'
+    },
+    sidebar: [
+			{
+				label: '[home] Home',
+				link: '/'
+			},
+			{
+				label: '[box] Frontend',
+				autogenerate: { directory: 'frontend', collapsed: true }
+			},
+			{
+				label: 'Backend',
+				autogenerate: { directory: 'backend', collapsed: true }
+			},
+			{
+				label: 'Devops',
+				autogenerate: { directory: 'devops', collapsed: true }
+			}
+		],
+		components: {
+      ThemeProvider: './src/components/ThemeProvider.astro',
+      ThemeSelect: './src/components/ThemeSelect.astro',
+      SiteTitle: './src/components/SiteTitle.astro',
+      Sidebar: './src/components/Sidebar.astro',
+      Pagination: './src/components/Pagination.astro',
+      Hero: './src/components/Hero.astro',
+    },
+    customCss: [
+			'@fontsource-variable/space-grotesk/index.css',
+      '@fontsource/space-mono/400.css',
+      '@fontsource/space-mono/700.css',
+			'./src/styles/tailwind.css',
+			'./src/styles/global.css',
+		],
+		expressiveCode: {
+      themes: ['github-dark']
+    },
+		pagination: false,
+    lastUpdated: true,
+
+  }),
+	tailwind({
+		// Disable the default base styles:
+		applyBaseStyles: false,
+	}),
+	]
+});
