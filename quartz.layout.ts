a7c3fa4b788e47bf1ea747f1e9a592347abd98e5
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-08-26 08:35:35 +0530

    Quartz sync: Aug 26, 2024, 8:35 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,6 +38,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
+    // Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
