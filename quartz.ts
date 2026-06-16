f5f13ae633f0dd9dc7f19776e0124e6041155f68
prnsh <58560660+pranshuj73@users.noreply.github.com>
2026-06-16 19:46:05 +0000

    update quartz site

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,9 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { registerCondition } from "./quartz/plugins/loader/conditions"
+
+const isIndexPage = (slug: string) => slug === "index" || slug.startsWith("index-")
+registerCondition("is-index-page", (props) => isIndexPage(props.fileData.slug ?? ""))
+registerCondition("not-index-page", (props) => !isIndexPage(props.fileData.slug ?? ""))
 
 const config = await loadQuartzConfig()
 export default config
